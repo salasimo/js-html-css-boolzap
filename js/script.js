cb47accc14ad76890ae1b8f3f42fac3f96f33fd3
Simone Sala <simonesala@me.com>
2020-03-05 14:55:46 +0000

    2° Milestone completata

--- js/script.js
+++ js/script.js
@@ -1,18 +1,50 @@
 $(document).ready(function(){
+    // if $(".message-field-text").val() !=
+    $(".enter-send").click(newMessageSent);
 
-    $(".enter-send").click(function(){
+$(".search-contacts").keyup(function(event){
+    var charFilter = $(this).val().toLowerCase();
+    $(".contact-name").each(function(){
+        if ($(this).text().toLowerCase().includes(charFilter)){
+            $(this).parents(".contact").show();
+        } else{
+            $(this).parents(".contact").hide();
+        }
+    });
+});
+
+// ========= FUNZIONI =======================
+
+    function addZero(i) {
+      if (i < 10) {
+        i = "0" + i;
+      }
+      return i;
+    };
+
+
+    function newMessageSent(){
         var input = $(".message-field-text").val();
         $(".message-field-text").val("");
         var time = new Date();
-        var messageSentTime = time.getHours().toString() + ":" + time.getMinutes().toString();
+        var messageTime = addZero(time.getHours(addZero)) + ":" + addZero(time.getMinutes());
         var message = $(".template-message-sent .message-sent").clone();
         message.find(".message-content").text(input);
-        message.find(".message-time").text(messageSentTime);
+        message.find(".message-time").text(messageTime);
         $(".messages-box").append(message);
-    });
+        $(".main-section").scrollTop($(".main-section")[0].scrollHeight); // funziona solo al primo click! Why??!
+        setTimeout(returnOk, 1000);
+    };
 
-
-
+    function returnOk(){
+        var time = new Date();
+        var messageTime = addZero(time.getHours(addZero)) + ":" + addZero(time.getMinutes());
+        var messageOk = $(".template-message-received .message-received").clone();
+        messageOk.find(".message-content").text("Ok");
+        messageOk.find(".message-time").text(messageTime);
+        $(".messages-box").append(messageOk);
+        $(".main-section").scrollTop($(".main-section")[0].scrollHeight); // funziona solo al primo click! Why??!
+    };
 
 
 
